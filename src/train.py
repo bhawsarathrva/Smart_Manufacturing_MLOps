b0601173a8d2da539d5421b96d6e1fa1c634192c
Athrva Bhawsar <athrvabh124@gmail.com>
2026-04-06 15:38:54 +0530

    update

--- src/train.py
+++ src/train.py
@@ -1,21 +1,46 @@
 import mlflow
+import os
+import joblib
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from logger import get_logger
+from custom_exception import CustomException
 
-mlflow.start_run()
+logger = get_logger(__name__)
 
-n_estimators = 100
-mlflow.log_param("n_estimators", n_estimators)
+def train():
+    try:
+        processed_path = "artifacts/processed"
+        logger.info("Loading processed data for training...")
+        
+        X_train = joblib.load(os.path.join(processed_path, "X_train.pkl"))
+        X_test = joblib.load(os.path.join(processed_path, "X_test.pkl"))
+        y_train = joblib.load(os.path.join(processed_path, "y_train.pkl"))
+        y_test = joblib.load(os.path.join(processed_path, "y_test.pkl"))
 
-model = RandomForestClassifier(n_estimators=n_estimators)
-model.fit(X_train, y_train)
+        mlflow.start_run()
 
-pred = model.predict(X_test)
+        n_estimators = 100
+        mlflow.log_param("n_estimators", n_estimators)
 
-accuracy = accuracy_score(y_test, pred)
+        # Train model
+        logger.info(f"Training RandomForest model with n_estimators: {n_estimators}")
+        model = RandomForestClassifier(n_estimators=n_estimators)
+        model.fit(X_train, y_train)
 
-mlflow.log_metric("accuracy", accuracy)
+        # Evaluate
+        pred = model.predict(X_test)
+        accuracy = accuracy_score(y_test, pred)
+        logger.info(f"Model Training Complete. Accuracy: {accuracy}")
 
-mlflow.sklearn.log_model(model, "model")
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.sklearn.log_model(model, "model")
 
-mlflow.end_run()
+        mlflow.end_run()
+
+    except Exception as e:
+        logger.error(f"Error occurred in train.py: {e}")
+        raise CustomException("Training failed in train.py", e)
+
+if __name__ == "__main__":
+    train()
